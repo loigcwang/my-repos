a3b9f348df5d8210431b4c7c2bda0758a7f7897a
logicwang <1758555877@qq.com>
2022-07-17 23:10:36 +0800

    添加deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+
+publicPath: process.env.NODE_ENV === 'production' ?
+    '/my-repos/' //这里写展示页面的仓库名称
+    :
+    '/'
