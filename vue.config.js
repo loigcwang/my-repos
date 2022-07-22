0bb5d44749e68055a5aa6ea360393f1cf884b24a
“logicwang” <1758555877@qq.com>
2022-07-22 22:17:46 +0800

    fix: modify vue config.

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/my-repos/' : '/'
 })
-
-publicPath: process.env.NODE_ENV === 'production' ?
-    '/my-repos/' //这里写展示页面的仓库名称
-    :
-    '/'
